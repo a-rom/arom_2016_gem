64462c285bb3d79751c7714fb7cc7919fb387203
Aritomo Moritoh <z14010am@aiit.ac.jp>
2016-07-30 08:23:08 +0000

    mori

--- lib/arom_2016_gem.rb
+++ lib/arom_2016_gem.rb
@@ -1,5 +1,12 @@
 require "arom_2016_gem/version"
 
 module Arom2016Gem
-  # Your code goes here...
+  class main
+    def odd?(n)
+      if(n%2)!=0
+        return true
+      else
+        return false
+      end
+    end
 end
